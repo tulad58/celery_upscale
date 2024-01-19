39bfe8d82e14f6694d4496ff2be9c571f0e29258
tulad58 <link7800@yandex.ru>
2024-01-19 18:29:05 +0600

    commit

--- tasks.py
+++ tasks.py
@@ -1,13 +1,23 @@
-from celery import Celery
-
-app = Celery('tasks', 
-            broker='redis://localhost:6379/0',
-            backend='redis://localhost:6379/1',
-            broker_connection_retry_on_startup=True)
+# from celery import Celery
+# from upscale.upscale1 import upscale
+# from app import app
 
 
-@app.task
-def upscale_image():
-    # result_filename = upscale('upscale.lama_300px.png', 'upscale.lama_300px.png', 'upscale.EDSR_x2.pb')
+# celery = Celery('tasks', 
+#             broker='redis://localhost:6379/0',
+#             backend='redis://localhost:6379/1',
+#             broker_connection_retry_on_startup=True)
 
-    return 'result_filename'
+
+# class ContextTask(celery.Task):
+#     def __call__(self, *args, **kwargs):
+#         with app.app_context():
+#             return self.run(*args, **kwargs)
+
+# celery.Task = ContextTask
+
+# @celery.task
+# def upscale_image():
+#     result_filename = upscale('upscale/lama_300px.png', 'upscale/lama_600px.png', 'upscale/EDSR_x2.pb')
+
+#     return result_filename
---
+++ upscale/upscale1.py
@@ -0,0 +1,16 @@
+
+
+# import sys
+# import os
+
+# sys.path.insert(0, os.path.dirname(__file__))
+
+
+
+
+def example():
+    upscale('lama_300px.png', 'lama_600px.png')
+
+
+if __name__ == '__main__':
+    example()
